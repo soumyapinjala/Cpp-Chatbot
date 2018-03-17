1777da3e8223eb6384063868d1d2464e25d390df
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-03-17 00:04:09 -0700

    Added the logic for all functions.

--- bot.cpp
+++ bot.cpp
@@ -1,34 +1,89 @@
 #pragma once
 #include "bot.h"
 
+
 bot::bot()
 {
 	std::ifstream inputFile;
 	inputFile.open("QA.txt");
 	std::string tmpQ; // used for temporarily storing questions from text file
 	std::string tmpA; // used for temporarily storing answer from text file
-	system("pause");
-	while (!inputFile.eof())
+
+	while (!inputFile.eof()) //loop until end of file is reached
 	{
-		getline(inputFile, tmpQ, ';');
-		getline(inputFile, tmpA);
-		question questionObj(tmpQ, tmpA);
-		questionList.push_back(questionObj);
-		std::cout << questionList.front().m_getQuery() << std::endl;
-		std::cout << questionList.front().m_getAnswer() << std::endl;
+		getline(inputFile, tmpQ, ';');	// read each line of file one at a time until end of file
+		getline(inputFile, tmpA);		// store questions and answers from file into two different variables
+		question questionObj(tmpQ, tmpA); // put variables in an object
+		questionList.push_back(questionObj); // store the object in a node in a doubly linked list
 	}
-
-	system("pause");
 }
 
-bool bot::isQuestion(std::string)
+bool bot::isQuestion(std::string input) // check if user input is formatted correctly ('?' at end of question return true if correct format, return false if inncorect format
 {
-	return true;
+	if (input.empty())
+	{
+		return false;
+	}
+	if (input.back() == '?')
+	{
+		return true;
+	}
+	else if (input.back() != '?')
+	{
+		return false;
+	}
 }
 
-bool bot::checkAndReply(std::string)
+bool bot::check(std::string input)
 {
-	return true;
+	std::list<question>::iterator it;
+	it = questionList.begin();
+
+	for (it; it != questionList.end(); it++)
+	{
+		if (input == it->m_getQuery())
+		{
+			return true;
+		}
+	}
+	return false;
+	
+}
+
+void bot::reply(std::string input)
+{
+	std::list<question>::iterator it;
+	it = questionList.begin();
+
+	for (it; it != questionList.end(); it++)
+	{
+		if (input == it->m_getQuery())
+		{
+			std::cout << it->m_getAnswer() << std::endl;
+		}
+	}
+}
+
+void bot::storeNewQuestion(std::string input)
+{
+	std::string tmp;
+	std::cout << "Hmmm . . . that's a good question! Maybe you could help me? " << std::endl << input << std::endl;
+	getline(std::cin, tmp); // ask user to answer the question
+	question new_query(input, tmp); // store new question and answer in an object
+	questionList.push_back(new_query); // store that object in a node inside of the doubly linked list
+	std::ofstream outputFile;
+	outputFile.open("QA.txt", std::ios::app);
+	if (outputFile.is_open())
+	{
+		outputFile << std::endl << input << ";" << tmp;
+	}
+	else
+	{
+		std::cout << "Error, file didn't open." << std::endl;
+	}
+	outputFile.close();
+
+
 }
 
 bot::~bot()
