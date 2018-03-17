7789293df9b53a6ecf7e18967dacc3eb4b327bce
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-03-16 17:59:07 -0700

    Update bot.cpp

--- bot.cpp
+++ bot.cpp
@@ -1,11 +1,34 @@
+
 #include "bot.h"
 
 bot::bot()
 {
+	std::ifstream inputFile;
+	inputFile.open("QA.txt");
+	std::string tmpQ; // used for temporarily storing questions from text file
+	std::string tmpA; // used for temporarily storing answer from text file
 
+	while (inputFile.is_open())
+	{
+		std::getline(std::cin, tmpQ, ';');
+		std::getline(std::cin, tmpA);
+		question questionObj(tmpA, tmpA);
+		questionList.push_back(questionObj);
+		std::cout << questionList.front().m_getQuery() << std::endl;
+		std::cout << questionList.front().m_getAnswer() << std::endl;
+	}
+
+	
+	
+	system("pause");
 }
 
-bool bot::checkQuestion()
+bool bot::isQuestion(std::string)
+{
+	return true;
+}
+
+bool bot::checkAndReply(std::string)
 {
 	return true;
 }
