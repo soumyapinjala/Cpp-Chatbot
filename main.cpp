b82b65f951b336e8de46d1586e74077d21c23849
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-03-16 17:59:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,16 @@ If the bot knows the answer to the question, the bot will reply with the answer
 If the bot does not know the answer to the question, the bot will ask the user the same question and store the answer for future use
 */
 
-#include <iostream>
-#include "question.h"
+
 #include "bot.h"
+#include "question.h"
+
 
 int main()
 {
+
+	bot bot();
+
 	while (true) 
 	{
 		std::string userInput;
