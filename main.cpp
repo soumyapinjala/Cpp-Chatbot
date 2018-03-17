6f43305a567ca4c2ae6757c64697f7d5c6e113bf
JackRMartin <36291367+JackRMartin@users.noreply.github.com>
2018-03-17 00:04:55 -0700

    Logic updates

--- main.cpp
+++ main.cpp
@@ -19,12 +19,34 @@ If the bot does not know the answer to the question, the bot will ask the user t
 int main()
 {
 
-	bot bot();
-
-	while (true) 
+	bot bot;
+	bool loop = true;
+	std::string userInput;
+	while (loop)
 	{
-		std::string userInput;
-		std::getline(std::cin, userInput);
+		std::cout << "Hello! Ask me a question!" << std::endl;
+		getline(std::cin, userInput);
+		for (int i = 0; i != userInput.length(); i++)
+		{
+			userInput.at(i) = tolower(userInput.at(i));
+		}
+		if (bot.isQuestion(userInput) == true)					// check if bot knows answer to the question.
+		{														// if bot knows the answer, reply with answer if bot does not know the answer then bot will ask the same question,
+			if (bot.check(userInput) == true)					//and store the users answer in the linked list as well as the text file	
+			{
+				bot.reply(userInput);
+			}
+			else
+			{
+				bot.storeNewQuestion(userInput);
+			}
+		}
+		else
+		{
+			std::cout << "You did not ask your question with the right format! Correct questions end with a '?'" << std::endl;
+		}
+		system("pause");
+		system("cls");
 	}
 
 
